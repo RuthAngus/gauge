5be642bb07c12f5cdf7d8eb1559439c13975caf2
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-12-09 22:29:00 +0000

    adding paths for bbq

--- granola/seismology/seismology.py
+++ granola/seismology/seismology.py
@@ -12,7 +12,7 @@ from gprot_fit import fit
 import kepler_data as kd
 
 
-def get_lc(id, KPLR_DIR="/Users/ruthangus/.kplr/data/lightcurves"):
+def get_lc(id, KPLR_DIR):
     """
     Downloads the kplr light curve and loads x, y and yerr.
     """
@@ -28,11 +28,15 @@ if __name__ == "__main__":
 
     # load van saders table
     DATA_DIR = "/Users/ruthangus/projects/granola/granola/data"
+    DATA_DIR = "/export/bbq2/angusr/granola/granola/data"
     data = pd.read_csv(os.path.join(DATA_DIR, "vanSaders.txt"))
     kics = data.KIC
 
+    KPLR_DIR = "/Users/ruthangus/.kplr/data/lightcurves"
+    KPLR_DIR = "/home/angusr/.kplr/data/lightcurves"
     for kic in kics:
-        x, y, yerr = get_lc(kic)
+        print("KepID = ", kic)
+        x, y, yerr = get_lc(kic, KPLR_DIR)
 
         # Use GProtation to get probabilistic rotation periods.
         gp = fit(x, y, yerr, kic)
