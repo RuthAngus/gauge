be0cf1f46c44bbe4f1afd726ecb71966e980ce0f
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-12-13 23:57:32 +0000

    using moving average for sigma clipping

--- granola/seismology/emcee2_gprot_fit.py
+++ granola/seismology/emcee2_gprot_fit.py
@@ -32,16 +32,24 @@ def make_gaps(x, y, yerr, points_per_day):
     return x[m][inds], y[m][inds], yerr[m][inds]
 
 
+def moving_average(y, window_size):
+    window = np.ones(int(window_size))/float(window_size)
+    return np.convolve(y, window, "same")
+
+
 def sigma_clip(x, y, yerr, nsigma):
-    med = np.median(y)
+    med = moving_average(y, 20)
     std = (sum((med - y)**2)/float(len(y)))**.5
+    print(std)
     m = np.abs(y - med) > (nsigma * std)
     return x[~m], y[~m], yerr[~m]
 
-def gp_fit(x, y, yerr, sid, RESULTS_DIR, p_max=None):
+
+def gp_fit(x, y, yerr, sid, RESULTS_DIR, burnin=1000, nwalkers=16, nruns=5,
+           full_run=1000, nsets=2, p_max=None, nsigma=3):
 
     # sigma clip
-    x, y, yerr = sigma_clip(x, y, yerr, 5)
+    x, y, yerr = sigma_clip(x, y, yerr, nsigma)
 
     ppd = 4  # cut off at 200 days, 4 points per day
     xb, yb, yerrb = make_gaps(x, y, yerr, ppd)
@@ -65,7 +73,6 @@ def gp_fit(x, y, yerr, sid, RESULTS_DIR, p_max=None):
     burnin, nwalkers, nruns, full_run = 1000, 16, 5, 1000
 
     assert p_init < np.exp(p_max), "p_init > p_max"
-
     # fast settings
 #     burnin, nwalkers, nruns, full_run = 2, 12, 2, 50
 #     xb[0], yb[0], yerrb[0] = xb[0][::100], yb[0][::100], yerrb[0][::100]
@@ -74,3 +81,22 @@ def gp_fit(x, y, yerr, sid, RESULTS_DIR, p_max=None):
     mcmc_fit(xb[:2], yb[:2], yerrb[:2], p_init, p_max, sid, RESULTS_DIR,
              truths=trths, burnin=burnin, nwalkers=nwalkers, nruns=nruns,
              full_run=full_run, diff_threshold=.5, n_independent=1000)
+
+if __name__ == "__main__":
+    import random
+    x = np.arange(0, 100, .1)
+    y = np.sin(2*np.pi*x*1./10)
+    err = .1
+    yerr = np.ones_like(y) * err
+    y += np.random.randn(len(x)) * err
+    for i in range(50):
+        y[np.random.choice(range(len(y)))] += .5
+    for i in range(50):
+        y[np.random.choice(range(len(y)))] -= .5
+    plt.clf()
+    plt.plot(x, y, "r.")
+    x2, y2, yerr2, med = sigma_clip(x, y, yerr, 2)
+    plt.plot(x2, y2, "k.")
+    print(len(x) - len(x2))
+    plt.plot(x, med)
+    plt.savefig("test")
