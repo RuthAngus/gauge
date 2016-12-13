01a6dff2e5587947d6d226753e7098e56d2d2aa6
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-12-13 15:17:26 +0000

    adding plot.py

---
+++ granola/seismology/plot.py
@@ -0,0 +1,26 @@
+# Create a plot of age vs Jz.
+# Create a plot of rotation period vs age.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import os
+
+DATA_DIR = "/export/bbq2/angusr/granola/granola/data"
+
+# load metcalfe and silva catalogues and merge.
+met = pd.read_csv(os.path.join(DATA_DIR, "metcalfe.csv"))
+sil = pd.read_csv(os.path.join(DATA_DIR, "silva_aguirre.csv"))
+
+print(met.keys())
+print(sil.keys())
+data = pd.merge(met, sil, on="KIC", how="right")
+print(data.keys())
+
+# load samples.
+
+
+# calculate their ages.
+
+# plot rotation period vs age
